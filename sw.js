d04f71b865f9cd1daaee624fc6cd7c75c819cef6
CBhobbit2156 <Christopherbolger@gmail.com>
2026-09-15 18:53:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
    Bump CACHE when you change index.html, or browsers will keep serving the
    old copy from cache. */
-var CACHE = 'food-diary-v9';
+var CACHE = 'food-diary-v10';
 var SHELL = [
   './',
   './index.html',
